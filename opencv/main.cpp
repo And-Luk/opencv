1e83be426c5c268ebb65a65d75a8994097318ef9
Andrei Lukashevich <And_Box@iCloud.com>
2025-10-04 07:47:47 +0200

    Initial Commit

---
+++ opencv/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  opencv
+//
+//  Created by Andrei Lukashevich on 04-10-2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
