9fa680e8a7ab5557363823ccb97cda2e2e5b813b
Andrei Lukashevich <And_Box@iCloud.com>
2025-10-04 12:55:23 +0200

    open the first webcam plugged in the computer

---
+++ opencv/header.h
@@ -0,0 +1,36 @@
+//
+//  header.h
+//  Verpackungsplan
+//
+//  Created by Andrei Lukashevich on 11-04-2025.
+//
+#pragma once
+
+
+#include <iostream>
+ 
+#include <fstream>
+#include <regex>
+#include <vector>
+#include <string>
+#include <filesystem>
+#include <cstring>
+
+#include <sstream>
+#include <deque>
+#include <map>
+#include <stdio.h>
+
+#include <ctime>
+
+#include <cstdlib> // "system"
+#include <chrono> // sleep for a second
+//#include <thread>
+
+
+
+
+
+
+using namespace std;
+using namespace filesystem;
--- opencv/main.cpp
+++ opencv/main.cpp
@@ -4,11 +4,36 @@
 //
 //  Created by Andrei Lukashevich on 04-10-2025.
 //
-
-#include <iostream>
+#include "header.h"
+#include <opencv2/opencv.hpp>
+//#include <>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
+    
+    // open the first webcam plugged in the computer
+       cv::VideoCapture camera(0); // in linux check $ ls /dev/video0
+       if (!camera.isOpened()) {
+           std::cerr << "ERROR: Could not open camera" << std::endl;
+           return 1;
+       }
+
+       // create a window to display the images from the webcam
+       cv::namedWindow("Webcam", cv::WINDOW_AUTOSIZE);
+
+       // array to hold image
+       cv::Mat frame;
+       
+       // display the frame until you press a key
+       while (1) {
+           // capture the next frame from the webcam
+           camera >> frame;
+           // show the image on the window
+           cv::imshow("Webcam", frame);
+           // wait (10ms) for esc key to be pressed to stop
+           if (cv::waitKey(10) == 27)
+               break;
+       }
+    
     std::cout << "Hello, World!\n";
     return EXIT_SUCCESS;
 }
